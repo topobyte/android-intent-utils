949c3795d02b964298e26ff86586869a67ac4ab9
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2014-09-06 11:55:42 +0200

    Initial project, copied src from android-map-utils

---
+++ src/de/topobyte/android/intent/IntentFactory.java
@@ -0,0 +1,95 @@
+// Copyright 2014 Sebastian Kuerten
+//
+// This file is part of android-intent-utils.
+//
+// android-intent-utils is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// android-intent-utils is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with android-intent-utils. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.android.maps.utils.intent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+public class IntentFactory {
+
+	public static Intent createUrlIntent(String url) {
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		return intent;
+	}
+
+	public static Intent createWikipediaIntent(String languageCode,
+			String pageName) {
+		String url = "http://" + languageCode + ".wikipedia.org/wiki/"
+				+ pageName;
+		return createUrlIntent(url);
+	}
+
+	public static Intent createTopobyteAppsIntent() {
+		String link = "market://search?q=pub:Topobyte.de";
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse(link));
+		return intent;
+	}
+
+	private static String mapsUrl = "http://spm.topobyte.de/apps";
+
+	public static Intent createTopobyteMapsIntent(Context context) {
+		String packageName = context.getApplicationContext().getPackageName();
+		String url = mapsUrl + "?id=" + packageName;
+		return IntentFactory.createUrlIntent(url);
+	}
+
+	public static Intent createTopobyteMapsIntent(Context context, String lang) {
+		String packageName = context.getApplicationContext().getPackageName();
+		String url = mapsUrl + "?lang=" + lang + "&id=" + packageName;
+		return IntentFactory.createUrlIntent(url);
+	}
+
+	public static Intent createAppDetailsIntent(String packageName) {
+		String link = "market://details?id=" + packageName;
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse(link));
+		return intent;
+	}
+
+	public static Intent createStadtplanNGAppDetailIntent(String identifier) {
+		String packageName = "de.topobyte.apps.offline.stadtplan." + identifier;
+		return createAppDetailsIntent(packageName);
+	}
+
+	public static Intent createThanksAppDetailIntent(ThankOption amount) {
+		String packageName;
+		switch (amount) {
+		default:
+		case THANK_1:
+			packageName = "de.topobyte.apps.thanks.amount1";
+			break;
+		case THANK_2:
+			packageName = "de.topobyte.apps.thanks.amount2";
+			break;
+		case THANK_5:
+			packageName = "de.topobyte.apps.thanks.amount5";
+			break;
+		case THANK_10:
+			packageName = "de.topobyte.apps.thanks.amount10";
+			break;
+		}
+		return createAppDetailsIntent(packageName);
+	}
+
+	public static Intent createRateAppIntent(Context context) {
+		String packageName = context.getApplicationContext().getPackageName();
+		return createAppDetailsIntent(packageName);
+	}
+}
---
+++ src/de/topobyte/android/intent/ThankOption.java
@@ -0,0 +1,26 @@
+// Copyright 2014 Sebastian Kuerten
+//
+// This file is part of android-intent-utils.
+//
+// android-intent-utils is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// android-intent-utils is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with android-intent-utils. If not, see <http://www.gnu.org/licenses/>.
+
+package de.topobyte.android.maps.utils.intent;
+
+public enum ThankOption {
+	THANK_RATE,
+	THANK_1,
+	THANK_2,
+	THANK_5,
+	THANK_10
+}
